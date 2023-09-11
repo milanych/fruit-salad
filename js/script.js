8550c9371b59b99ce716f5ed0d9056eed8df7de1
Milanych <vadimzotov.uk@gmail.com>
2023-09-11 15:02:17 +0100

    fetching data

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 const fruitForm = document.querySelector('#inputSection form');
 const fruitList = document.querySelector('#fruitSection ul');
+const fruitNutrition = document.querySelector('#nutritionSection');
 
-const addFruit = (fruit, list) => {
+const fetchFruitData = (fruit) => {
+  fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      addFruit(data)
+    })
+    .catch(e => console.error(e))
+}
+let calories = 0;
+let carbohydrates = 0;
+let fat = 0;
+let protein = 0;
+let sugar = 0;
+
+const addFruit = (fruit) => {
   if (!fruit) {
     return null;
   }
   const li = document.createElement('li');
-  li.textContent = fruit;
-  list.appendChild(li)
+  li.textContent = fruit.name;
+  fruitList.appendChild(li)
+
+
+  calories += fruit.nutritions.calories;
+  fat += fruit.nutritions.fat;
+  carbohydrates += fruit.nutritions.carbohydrates;
+  protein += fruit.nutritions.protein;
+  sugar += fruit.nutritions.sugar;
+
+  fruitNutrition.textContent = `Calories: ${calories.toFixed(2)} Carbohydrates: ${fruit.nutritions.carbohydrates.toFixed(2)} Fat: ${fat.toFixed(2)} Protein: ${protein.toFixed(2)} Sugar: ${fruit.nutritions.sugar.toFixed(2)}`
 }
 
-const removeFruit = e => {
-  console.log(e.target)
-  e.target.remove();
-}
-fruitList.addEventListener('click', removeFruit)
+fruitList.addEventListener('click', (e) => {
+  console.log(e)
+  e.target.remove()
+})
 
 fruitForm.addEventListener('submit', (el) => {
   el.preventDefault();
-  addFruit(el.target.fruitInput.value, fruitList);
+  fetchFruitData(el.target.fruitInput.value, fruitList)
   el.target.fruitInput.value = '';
 })
