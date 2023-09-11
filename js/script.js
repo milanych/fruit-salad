70012e979d4fbb9e7857ce5c45c0f2a4701fd2a4
Milanych <vadimzotov.uk@gmail.com>
2023-09-11 12:06:40 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 const fruitForm = document.querySelector('#inputSection form');
+const fruitList = document.querySelector('#fruitSection ul');
 
-console.log(fruitForm)
+const addFruit = (fruit, list) => {
+  if (!fruit) {
+    return null;
+  }
+  const li = document.createElement('li');
+  li.textContent = fruit;
+  list.appendChild(li)
+}
+
+const removeFruit = e => {
+  console.log(e.target)
+  e.target.remove();
+}
+fruitList.addEventListener('click', removeFruit)
+
+fruitForm.addEventListener('submit', (el) => {
+  el.preventDefault();
+  addFruit(el.target.fruitInput.value, fruitList);
+  el.target.fruitInput.value = '';
+})
